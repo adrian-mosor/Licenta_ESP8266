08e56c845b8ccd5a04006f8b9680fc0ee3586f2d
adrian-mosor <mosoradrian123@gmail.com>
2023-04-01 18:51:29 +0300

    Dynamic ESP IP feature

--- include/wireless_conn.h
+++ include/wireless_conn.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266HTTPClient.h>
+
+extern WiFiClient client;
 
 // Set WiFi credentials
 
@@ -8,7 +12,9 @@
 // #define WIFI_PASS "Soft@Lasting123"
 
 #define WIFI_SSID "303B"
-#define WIFI_PASS "adi123456"   //IP address: 192.168.0.101 
-
+#define WIFI_PASS "adi123456"   
 
 void setup_Wifi();
+
+// Object ID String for dweet: smart_matrix_esp8266
+void send_IP_to_dweet(String ESP8266IP);
--- src/wireless_conn.cpp
+++ src/wireless_conn.cpp
@@ -1,30 +1,58 @@
 #include "wireless_conn.h"
 
-void setup_Wifi(){
+void setup_Wifi()
+{
 
-  // Setup serial port
-  Serial.begin(9600);
-  Serial.println();
+    // Setup serial port
+    Serial.begin(9600);
+    Serial.println();
 
-  delay(1000);
-  // Begin WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
+    delay(1000);
+    // Begin WiFi
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 
-  // Connecting to WiFi...
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-  // Loop continuously while WiFi is not connected
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(100);
-    Serial.print(".");
-  }
+    // Connecting to WiFi...
+    Serial.print("Connecting to ");
+    Serial.print(WIFI_SSID);
+    // Loop continuously while WiFi is not connected
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
 
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
+    WiFi.setAutoReconnect(true);
+    WiFi.persistent(true);
 
-  // Connected to WiFi
-  Serial.println();
-  Serial.print("Connected! IP address: (new function)");
-  Serial.println(WiFi.localIP());
+    // Connected to WiFi
+    Serial.println();
+    Serial.print("Connected! IP address: (new function)");
+    Serial.println(WiFi.localIP());
+    send_IP_to_dweet(WiFi.localIP().toString());
+}
+
+void send_IP_to_dweet(String ESP8266IP)
+{
+
+    HTTPClient http;
+    String dweet_url = "http://dweet.io/dweet/for/smart_matrix_esp8266?ip=";
+    dweet_url += ESP8266IP;
+
+    http.begin(client, dweet_url);
+    int httpCode = http.GET();
+
+    if (httpCode > 0)
+    {
+
+        String payload = http.getString();
+        Serial.println("Dweet.io response:");
+        Serial.println(payload);
+    }
+    else
+    {
+
+        Serial.println("Error sending data to dweet.io");
+    }
+
+    http.end();
 }
