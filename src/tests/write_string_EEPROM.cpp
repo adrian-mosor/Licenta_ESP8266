742a6d5ec86f88dbb257f0b431300b70c1826e7f
adrian-mosor <mosoradrian123@gmail.com>
2023-04-23 12:40:10 +0300

    Adapted EEPROM unit test for production code

--- src/tests/write_string_EEPROM.cpp
+++ src/tests/write_string_EEPROM.cpp
@@ -1,20 +1,68 @@
 #include <EEPROM.h>
 #include <Arduino.h>
 
-#define ADRESS_TEST 150
-#define FIXED_SIZE 6    //qsid length
+#define ADRESS_TEST 100
+#define FIXED_SIZE 6 // qsid length
 
-void writeEEPROM() {
+void writeEEPROM(int offset, String str)
+{
+    // EEPROM.begin(100);
+    EEPROM.put(offset, str);
+    EEPROM.commit(); // save changes to EEPROM
+
+    Serial.print("Wrote to EEPROM: ");
+    Serial.println(str);
+
+    // EEPROM.end();
+}
+
+void readEEPROM(int offset)
+{
+    // EEPROM.begin(100);
+
+    String strread;
+    EEPROM.get(offset, strread);
+    strread = strread.substring(0, FIXED_SIZE);
+
+    Serial.print("Extracted length from EEPROM: ");
+    Serial.println(strread.length());
+    Serial.print("Extracted string from EEPROM: ");
+    Serial.println(strread);
+
+    // EEPROM.end();
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    EEPROM.begin(128); // Initialize EEPROM with the required size
+
+    // writeEEPROM();
+    // readEEPROM();
+
+    String qsid = "Welc20";
+    writeEEPROM(ADRESS_TEST, qsid);
+    readEEPROM(ADRESS_TEST);
+}
+
+void loop()
+{
+}
+
+// raw functions:
+void writeEEPROM()
+{
     String qsid = "Global";
 
     Serial.println("writing eeprom ssid:");
     EEPROM.put(ADRESS_TEST, qsid);
-    EEPROM.commit(); // Save the changes to EEPROM
+    EEPROM.commit();
     Serial.print("Wrote: ");
     Serial.println(qsid);
 }
 
-void readEEPROM() {
+void readEEPROM()
+{
     Serial.println("Reading EEPROM ssid");
     String esid;
 
@@ -24,15 +72,4 @@ void readEEPROM() {
     Serial.println(esid.length());
     Serial.print("SSID: ");
     Serial.println(esid);
-}
-
-void setup() {
-    Serial.begin(9600);
-    EEPROM.begin(512); // Initialize EEPROM with the required size
-
-    writeEEPROM();
-    readEEPROM();
-}
-
-void loop() {
 }
