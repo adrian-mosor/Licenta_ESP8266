8a4064e8fab331293ab98a0a8062acda7b283879
adrian-mosor <mosoradrian123@gmail.com>
2023-05-08 18:48:37 +0300

    Stable version 1.0

--- include/wireless_conn.h
+++ include/wireless_conn.h
@@ -8,11 +8,11 @@ extern WiFiClient client;
 
 // Set WiFi credentials
 
-#define WIFI_SSID "Lasting-Software"
-#define WIFI_PASS "Soft@Lasting123"
+// #define WIFI_SSID "Lasting-Software"
+// #define WIFI_PASS "Soft@Lasting123"
 
-// #define WIFI_SSID "303B"
-// #define WIFI_PASS "adi123456"   
+#define WIFI_SSID "303B"
+#define WIFI_PASS "adi123456"   
 
 void setup_Wifi();
 
